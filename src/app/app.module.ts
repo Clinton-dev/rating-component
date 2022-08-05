cbed8db383eeff68628edc025c3efb5211c798a1
Clinton wambugu <clintonwambugu@gmail.com>
2022-08-05 22:42:32 +0300

    Add routing to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { RatingComponent } from './rating/rating.component';
 import { ThankyouComponent } from './thankyou/thankyou.component';
+
+const appRoutes: Routes = [
+  { path: '', component: RatingComponent },
+  { path: 'thankyou', component: ThankyouComponent },
+  { path: '**', component: RatingComponent },
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +21,8 @@ import { ThankyouComponent } from './thankyou/thankyou.component';
     ThankyouComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
