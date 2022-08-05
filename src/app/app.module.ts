aff4eb488a313dce95e7bd89faf275e3eb76ed67
Clinton wambugu <clintonwambugu@gmail.com>
2022-08-05 20:29:21 +0300

    Add thankyou component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { RatingComponent } from './rating/rating.component';
+import { ThankyouComponent } from './thankyou/thankyou.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    RatingComponent
+    RatingComponent,
+    ThankyouComponent
   ],
   imports: [
     BrowserModule
